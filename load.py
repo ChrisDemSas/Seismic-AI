ff714278322ef300e613a993a037f274f7ed4952
ChrisDemSas <christopher.sastropranoto@gmail.com>
2024-07-26 17:45:51 +0800

    Started on load functions

--- load.py
+++ load.py
@@ -5,3 +5,21 @@
 ###################################################################
 
 import pandas as pd
+import os
+import utils.utilities as ut
+import utils.aws as aws
+import utils.bigquery as bq
+
+def upload_to_s3(filepath: str) -> None:
+    """Take in a filepath and upload this to AWS S3.
+    
+    Attributes:
+        filepath: The filepath of the data.
+    """
+
+def upload_to_bigquery(filepath: str) -> None:
+    """Take in a filepath and upload this to BigQuery.
+    
+    Attributes:
+        filepath: The filepath of the data.
+    """
