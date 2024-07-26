7bdf7a5ebefbd596da344d3085f4519f1c288a13
ChrisDemSas <christopher.sastropranoto@gmail.com>
2024-07-26 18:04:51 +0800

    Load file written

--- load.py
+++ load.py
@@ -10,7 +10,7 @@ import utils.utilities as ut
 import utils.aws as aws
 import utils.bigquery as bq
 
-def upload_to_s3(parameters: dict, client: aws.AWSClient, html: bool = True, csv: bool = True) -> None:
+def upload_to_s3(client: aws.AWSClient, parameters: dict, html: bool = True, csv: bool = True) -> None:
     """Take in a filepath and upload this to AWS S3.
     
     Attributes:
@@ -22,18 +22,22 @@ def upload_to_s3(parameters: dict, client: aws.AWSClient, html: bool = True, csv
         for path in os.listdir("data/html"):
             filepath = f'data/html/{path}'
             client.upload_file(filepath, parameters)
+            ut.delete_file(filepath)
 
     else:
         for path in os.listdir("data/csv"):
             filepath = f'data/csv/{path}'
             client.upload_file(filepath, parameters)
+            ut.delete_file(filepath)
 
-
-def upload_to_bigquery(parameters: dict, client: ) -> None:
+def upload_to_bigquery(client: bq.BigQueryClient, dataframe: pd.DataFrame, table_id: str) -> None:
     """Take in a filepath and upload this to BigQuery.
     
     Attributes:
         filepath: The filepath of the data.
     """
 
-print(upload_to_s3('s', {'s': 1}, None))
+    result = client.load(dataframe, table_id)
+
+
+
